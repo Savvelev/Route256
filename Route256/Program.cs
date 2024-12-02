72370978d0957b77e7f23c0b5b8779e9e7047da2
savve <saveliev.m.v@yandex.ru>
2024-12-02 18:21:00 +0500

    feat: (#1.1): Add DI

--- Route256/Program.cs
+++ Route256/Program.cs
@@ -1,9 +1,17 @@
+using Route256.BLL.DeliveryPrice;
+using Route256.DAL.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IDeliveryPriceService, DeliveryPriceService>();
+builder.Services.AddScoped<IStorageRepository, StorageRepository>();
 
 var app = builder.Build();
 
@@ -16,7 +24,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.MapControllers();
 
 app.Run();
 
